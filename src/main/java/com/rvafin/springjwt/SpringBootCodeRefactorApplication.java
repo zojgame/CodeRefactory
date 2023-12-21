d0e2579656ab8ee561f0e5d53a43f595b5a476b4
VafinRazil <75613021+VafinRazil@users.noreply.github.com>
2023-12-21 17:11:20 +0300

    Add files

---
+++ src/main/java/com/rvafin/springjwt/SpringBootCodeRefactorApplication.java
@@ -0,0 +1,13 @@
+package com.rvafin.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class SpringBootCodeRefactorApplication {
+
+	public static void main(String[] args) {
+    SpringApplication.run(SpringBootCodeRefactorApplication.class, args);
+	}
+
+}
